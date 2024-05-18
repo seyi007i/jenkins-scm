b0129606c3203f446428ff630c81e9c8e3379dc9
seyi ogunniran <ogunniranseyi@yahoo.com>
2024-05-18 12:57:24 +0100

    modified webserver script for apache

--- websetup.sh
+++ websetup.sh
@@ -4,31 +4,26 @@
 echo "########################################"
 echo "Installing packages."
 echo "########################################"
-sudo apt install wget unzip apache2 -y > /dev/null
+sudo apt update -y
+sudo apt install wget unzip apache2 -y >/dev/null
 echo
-
-# Start & Enable Service
-echo "########################################"
-echo "Start & Enable Apache2 Service"
-echo "########################################"
-
 
 # Creating Temp Directory
 echo "########################################"
 echo "Starting Artifact Deployment"
 echo "########################################"
-mkdir -p /tmp/webfiles2
-cd /tmp/webfiles2
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
 echo
 
-wget https://www.tooplate.com/zip-templates/2098_health.zip > /dev/null
-unzip 2098_health.zip > /dev/null
-sudo cp -r 2098_health/* /var/www/html/
+wget https://www.tooplate.com/zip-templates/2131_wedding_lite.zip > /dev/null
+unzip 2131_wedding_lite.zip > /dev/null
+sudo cp -r 2131_wedding_lite/* /var/www/html/
 echo
 
 # Bounce Service
 echo "########################################"
-echo "Restarting apache2 service"
+echo "Restarting Apache Webserver service"
 echo "########################################"
 systemctl restart apache2
 echo
@@ -37,8 +32,9 @@ echo
 echo "########################################"
 echo "Removing Temporary Files"
 echo "########################################"
-rm -rf /tmp/webfiles2
+rm -rf /tmp/webfiles
 echo
 
-sudo systemctl status apache2
 ls /var/www/html/
+sudo systemctl status apache2
+
