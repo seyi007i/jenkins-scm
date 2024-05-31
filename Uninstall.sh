0980a9d36f1fff37fd80db6001436afac8c4e57a
seyi ogunniran <ogunniranseyi@yahoo.com>
2024-05-31 11:41:43 +0100

    docker build

--- Uninstall.sh
+++ Uninstall.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # CentOS
-sudo systemctl stop httpd
-sudo rm -rf /var/www/html/*
-sudo yum remove httpd wget unzip -y
-
+#sudo systemctl stop httpd
+#sudo rm -rf /var/www/html/*
+#sudo yum remove httpd wget unzip -y 
 # Ubuntu
 #!/bin/bash
 sudo systemctl stop apache2
 sudo rm -rf /var/www/html/*
-sudo apt autoremove apache2 wget unzip -y
+sudo apt autoremove apache2  -y
